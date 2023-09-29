f7e6b4d7b449833fcd5a057f75450a26ef7f1ab9
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-09-29 19:59:42 +0700

    Update page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime'
+import "popper.js"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './styles/main.css'
+import main from './scripts/main'
+
+main()
